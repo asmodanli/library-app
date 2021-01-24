9b5664a871b2dc5016858aabe60a91a640eed970
Sena <a.modanlioglu@ogr.iu.edu.tr>
2021-01-24 23:41:24 +0300

    remove unnecessary functions

--- src/main/java/com/sign/demo/dao/BookRepository.java
+++ src/main/java/com/sign/demo/dao/BookRepository.java
@@ -6,8 +6,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface BookRepository extends JpaRepository<Book, Integer> {
-
-    public List<Book> findAllByOrdOrderByName();
-    public void deleteBookByIsbn(int isbn);
-    public Book findByIsbn(int isbn);
 }
