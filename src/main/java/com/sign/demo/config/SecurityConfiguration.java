22d79b06514dd7c15f5d85f50f1e4a350d02bac0
Sena <a.modanlioglu@ogr.iu.edu.tr>
2021-01-27 22:58:34 +0300

    admin authentication

--- src/main/java/com/sign/demo/config/SecurityConfiguration.java
+++ src/main/java/com/sign/demo/config/SecurityConfiguration.java
@@ -23,6 +23,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception{
         http
                 .authorizeRequests()
+                .antMatchers("/admin/**").hasRole("ADMIN")
                 .antMatchers(
                         "/registration**",
                         "/js/**",
