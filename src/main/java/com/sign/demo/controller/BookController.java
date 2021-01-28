69c5a39243ec5e56e0ecf6b9d82836276d4f8e16
Sena <a.modanlioglu@ogr.iu.edu.tr>
2021-01-29 00:06:24 +0300

    user part added

--- src/main/java/com/sign/demo/controller/BookController.java
+++ src/main/java/com/sign/demo/controller/BookController.java
@@ -1,8 +1,11 @@
 package com.sign.demo.controller;
 
 import com.sign.demo.entity.Book;
+import com.sign.demo.entity.Writer;
 import com.sign.demo.service.BookService;
+import com.sign.demo.service.WriterService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -10,29 +13,45 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @Controller
-@RequestMapping("/books")
+@RequestMapping("/admin")
 public class BookController {
 
     @Autowired
     private BookService bookService;
 
-    @GetMapping("/list")
-    public String listBooks(Model theModel) {
-        theModel.addAttribute("listBooks", bookService.getAllBooks());
-        return "books/list-books";
+    @Autowired
+    private WriterService writerService;
+
+    @GetMapping("/listBooks")
+    public String viewBookPage(Model theModel, String isbn) {
+
+        if(isbn != null) {
+            theModel.addAttribute("listBooks", bookService.findByIsbn(isbn));
+        }
+        else if(isbn == ""){
+            theModel.addAttribute("listBooks", bookService.getAllBooks());
+        }
+        else{
+            theModel.addAttribute("listBooks", bookService.getAllBooks());
+        }
+
+        return "books/book";
     }
+
 
     @GetMapping("/showNewBookForm")
     public String showNewBookForm(Model model) {
         Book book = new Book();
         model.addAttribute("book", book);
-        return "new_book";
+        model.addAttribute("listWriters", writerService.getAllWriters());
+        return "books/add-book";
     }
 
     @PostMapping("/saveBook")
-    public String saveBook(@ModelAttribute("save") Book theBook) {
+    public String saveBook(@ModelAttribute("book") Book theBook) {
+
         bookService.saveBook(theBook);
-        return  "redirect:/books/list";
+        return  "redirect:/admin/listBooks";
     }
 
     @GetMapping("/showFormForUpdate/{id}")
@@ -40,7 +59,7 @@ public class BookController {
         Book book = bookService.getBookById(id);
         model.addAttribute("book", book);
 
-        return "update_book";
+        return "books/update-book";
     }
 
     @GetMapping("/deleteBook/{id}")
@@ -48,8 +67,9 @@ public class BookController {
 
         this.bookService.deleteBookById(id);
 
-        return "redirect:/";
+        return "redirect:/admin/listBooks";
     }
+
 
 
 }
